89761c12a93e5ed0af9a0ab935bd67902d385264
Hsiao Yao <tomtogo1260@gmail.com>
2024-06-24 17:20:41 +0800

    start to split each line data

--- handler/OBJLoader.js
+++ handler/OBJLoader.js
@@ -2,6 +2,20 @@ import { Loader } from './Loader';
 
 import { FileLoader } from 'three';
 
+const _face_vertex_data_separator_pattern = /\s+/;
+
+function ParserState() {
+
+    const state = {
+        vertices: [],
+        normals: [],
+        colors: [],
+        uvs: []
+    }
+
+    return state
+}
+
 class OBJLoader extends Loader {
 
     constructor( manager ) {
@@ -27,6 +41,24 @@ class OBJLoader extends Loader {
     }
 
     parse( text ) {
+        const state = new ParserState();
+
+        const lines = text.split( '\n' );
+
+        for ( let i = 0, l = lines.length; i < l; i ++ ) {
+
+            const line = lines[ i ].trimStart();
+
+            if ( line.length === 0 ) continue;
+
+            const lineFirstChar = line.charAt( 0 );
+
+            const data = line.split( _face_vertex_data_separator_pattern );
+
+            console.log('char: ', lineFirstChar, 'line: ', line);
+            console.log('split: ', data)
+        }
+
         return text
     }
 
--- readme.md
+++ readme.md
@@ -18,6 +18,12 @@ You can see the console panel output contains loader & exporter info.
 
 Three.js 的 Loader 大多是基於 `Loader.js` 的這個 class 去實作的，沒指定 manager 則會用預設的 `LoadingManager.js`
 
-### OBJLoader
+## OBJLoader
 
-載入 `.obj` 檔，呼叫 load 方法時，第一個參數是檔案路徑，第二個是 onLoad 函數，其輸入就是解析後的結果。load 方法會創建 FileLoader 的類實例，這個類實例的 load 方法會把 `.obj` 以字串的方式作為輸入，這個字串會傳給 parse 方法做解析。
+#### load
+
+載入 `.obj` 檔，呼叫 load 方法時，第一個參數是檔案路徑，第二個是 onLoad 函數，其輸入就是解析後的結果。load 方法會創建 FileLoader 的類實例，這個類實例的 load 方法會把 `.obj` 以字串的方式作為輸入，這個字串會傳給 parse 方法做解析
+
+#### parse
+
+ParserState 方法負責管理一些狀態，好比說 vertices, uvs 等資料。下面是一系列對字串的處理，首先是把不同行用 split 分開，然後再把每行透過 trimStart 去掉前面的空白，lineFirstChar 是每行的第一個字母，用來做一些判斷，_face_vertex_data_separator_pattern 則負責把每行用 space 分開
