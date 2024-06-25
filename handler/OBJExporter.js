515486e4ec32926d5a7defc32e7c592c2df60680
Hsiao Yao <tomtogo1260@gmail.com>
2024-06-25 14:53:51 +0800

    export vertices info

--- handler/OBJExporter.js
+++ handler/OBJExporter.js
@@ -1,9 +1,51 @@
+import {
+    Color,
+    Matrix3,
+    Vector2,
+    Vector3
+} from 'three';
+
 class OBJExporter {
     parse( object ) {
 
+        let output = '';
+
+        const vertex = new Vector3();
+
         function parseMesh( mesh ) {
 
-            console.log('traverse & find a mesh', mesh);
+            let nbVertex = 0;
+
+            const geometry = mesh.geometry;
+
+            // shortcuts
+            const vertices = geometry.getAttribute( 'position' );
+            const normals = geometry.getAttribute( 'normal' );
+            const uvs = geometry.getAttribute( 'uv' );
+            const indices = geometry.getIndex();
+
+            console.log('vertices: ', vertices.array);
+            console.log('normals: ', normals.array);
+            console.log('uvs: ', uvs.array);
+            console.log('indices: ', indices);
+
+            // vertices
+
+            if ( vertices !== undefined ) {
+
+                for ( let i = 0, l = vertices.count; i < l; i ++, nbVertex ++ ) {
+
+                    vertex.fromBufferAttribute( vertices, i );
+
+                    // transform the vertex to world space
+                    vertex.applyMatrix4( mesh.matrixWorld );
+
+                    // transform the vertex to export format
+                    output += 'v ' + vertex.x + ' ' + vertex.y + ' ' + vertex.z + '\n';
+
+                }
+
+            }
 
         }
 
@@ -16,6 +58,8 @@ class OBJExporter {
             }
 
         } );
+
+        return output
     }
 }
 
